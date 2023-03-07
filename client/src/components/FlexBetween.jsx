ead45ecebac0823468cf843435d72b1b2cd0662d
turnPenny <bohdan.miroshnik23@gmail.com>
2023-03-07 18:48:58 +0100

    navbar

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,8 +1,10 @@
-const { Box } = require('@mui/material');
-const { styled } = require('@mui/system');
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
 
-export default flexBetween = styled(Box)({
+const FlexBetween = styled(Box)({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
 });
+
+export default FlexBetween;
--- client/src/sections/Layout/index.jsx
+++ client/src/sections/Layout/index.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import { Box, useMediaQuery, useTheme } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import Navbar from 'components/Navbar';
 
 const Layout = () => {
-  const dispatch = useDispatch();
-  const theme = useTheme();
   return (
     <Box width='100%' height='100%'>
       <Box>
