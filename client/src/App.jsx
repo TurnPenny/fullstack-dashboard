51ffff403f4c3893830e23e05bffa075fc106ba1
turnPenny <bohdan.miroshnik23@gmail.com>
2023-03-07 17:17:02 +0100

    layout component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,28 @@ import { createTheme } from '@mui/material';
 import { useMemo } from 'react';
 import { themeSettings } from './theme.js';
 import { useSelector } from 'react-redux';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from 'sections/Dashboard';
+import Layout from 'sections/Layout';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className='App'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {/* routes */}
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to='/dashboard' replace />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+          {/* routes */}
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,8 @@
+const { Box } = require('@mui/material');
+const { styled } = require('@mui/system');
+
+export default flexBetween = styled(Box)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
